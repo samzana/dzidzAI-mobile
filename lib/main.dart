bb51c752439a0f78622c0790b556bf83ac6310d4
samzana <samuelzana18@gmail.com>
2024-04-28 11:54:13 +0200

    configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -108,9 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
-              style: TextStyle(
-                fontFamily: 'Baloo 2'
-              ),
+              style: TextStyle(fontFamily: 'Baloo 2'),
             ),
             Text(
               '$_counter',
